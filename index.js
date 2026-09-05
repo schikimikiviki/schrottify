98a28c451c5f524bfe3a083044e74f3b75d7fe3a
schikimikiviki <viktoria.bzdyk@gmx.at>
2026-09-05 17:53:02 +0200

    updated frontend struggle with express

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import bodyParser from 'body-parser';
 import express from 'express'
 import {dirname, join} from 'path';
 import {fileURLToPath} from 'url';
@@ -12,35 +13,55 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(join(__dirname, 'public')))
 
-app.use('/webtui', express.static(join(__dirname, 'node_modules/@webtui/css/dist')));
+app.use(
+    '/webtui',
+    express.static(join(__dirname, 'node_modules/@webtui/css/dist')));
 
 app.use('/', function(req, res) {
   res.sendFile(join(__dirname, 'public/index.html'));
 });
 
+app.get('/submit', (req, res) => {
+  const data = req.query;
+  console.log(data);
+  res.send(`<h1 style="text-align: center; 
+    margin-top: 50vh; transform: translateY(-50%);">
+    Form submitted successfully!</h1>`);
+});
+
+app.get('/download-spotify', async (req, res) => {
+  const data = req.body;
+  console.log(data);
+  console.log(req)
+  console.log('SPOTIFY DOWNLOAD START')
+
+  let youtubeUrl = url;
+
+  console.log('Url: ' + youtubeUrl)
+  // let
+  // playListID = '5ATbARi8XEvqgvxdUZldFb'
+
+  // let token = await getAccessToken();
+  // let songs = await getSongsFromPlayListWithID(playListID, token);
+
+
+  // // for (let i in songs) {
+  // //   console.log(songs[i])
+  // // }
+
+  // let query = 'Sprawling Idiot Effigy - Neros Day At Disneyland';
+  // let res = await fetchSongInfo(query);
+
+  // let youtubeID = res['items'][0]['id']['videoId'];
+  // let youtubeURL = `https://www.youtube.com/watch?v=${youtubeID}`
+  // console.log(youtubeURL)
+
+  //  downloadMp3(youtubeUrl);
+});
 
 app.listen(3000);
-
-let youtubeUrl = 'https://www.youtube.com/watch?v=Jklg-ivTHYQ&start_radio=1';
-let playListID = '5ATbARi8XEvqgvxdUZldFb'
-
-let token = await getAccessToken();
-// console.log('Credentials: ', await token)
-
-let songs = await getSongsFromPlayListWithID(playListID, token);
-
-
-// for (let i in songs) {
-//   console.log(songs[i])
-// }
-
-let query = 'Sprawling Idiot Effigy - Neros Day At Disneyland';
-let res = await fetchSongInfo(query);
-
-let youtubeID = res['items'][0]['id']['videoId'];
-let youtubeURL = `https://www.youtube.com/watch?v=${youtubeID}`
-console.log(youtubeURL)
-
-    //  downloadMp3(youtubeUrl);
---
+++ public/script.js
@@ -0,0 +1,23 @@
+// const element = document.querySelector('#spotify-form');
+// const button = element?.querySelector('button');
+
+// if (button) {
+//   button.addEventListener('submit', async (event) => {
+//     event.preventDefault();
+
+//     const url = event.currentTarget.elements.url.value;
+
+//     console.log(url);
+//     console.log('sccknael');
+//   })
+// }
+
+// if (element) {
+//   element.addEventListener('submit', async (event) => {
+//     event.preventDefault();
+
+//     const url = event.currentTarget.elements.url.value;
+
+//     console.log(url)
+//   })
+// }
